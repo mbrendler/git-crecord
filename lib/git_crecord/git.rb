d3f819ae6da73dbf5ea8740c37a62dafc0d28320
Maik Brendler <maik.brendler@injixo.com>
2016-04-21 18:58:25 +0200

    Clean up

--- lib/git_crecord/git.rb
+++ lib/git_crecord/git.rb
@@ -1,36 +1,23 @@
 module GitCrecord
   module Git
-# int stage(const Files& files) {
-#     logger() << "stage" << std::endl;
-#     std::stringstream s;
-#     for (auto f : files) {
-#         f->generate_diff(s);
-#     }
-#     FILE* cmd = popen("git apply --cached --unidiff-zero - ", "w");
-#     const int rc = fputs(s.str().c_str(), cmd);
-#     logger() << "fputs: " << rc << std::endl;
-#     logger() << s.str().c_str() << std::endl;
-#     const int status_code = pclose(cmd);
-#     logger() << "pclose: " << rc << std::endl;
-#     return status_code;
-# }
-#
-# int commit() {
-#     return system("git commit");
-# }
-#
-# std::string get_diff() {
-#     logger() << "get_diff" << std::endl;
-#     std::stringstream s;
-#     char buffer[1000];
-#     FILE* cmd = popen("git diff --no-ext-diff --no-color", "r");
-#     while (fgets(buffer, sizeof(buffer) / sizeof(*buffer), cmd)) {
-#         s << buffer;
-#     }
-#     const int rc = pclose(cmd);
-#     logger() << "pclose: " << rc << std::endl;
-#     return s.str();
-# }
+    # int stage(const Files& files) {
+    #     logger() << "stage" << std::endl;
+    #     std::stringstream s;
+    #     for (auto f : files) {
+    #         f->generate_diff(s);
+    #     }
+    #     FILE* cmd = popen("git apply --cached --unidiff-zero - ", "w");
+    #     const int rc = fputs(s.str().c_str(), cmd);
+    #     logger() << "fputs: " << rc << std::endl;
+    #     logger() << s.str().c_str() << std::endl;
+    #     const int status_code = pclose(cmd);
+    #     logger() << "pclose: " << rc << std::endl;
+    #     return status_code;
+    # }
+    #
+    # int commit() {
+    #     return system("git commit");
+    # }
 
     def self.diff
       `git diff --no-ext-diff --no-color`
@@ -39,13 +26,5 @@ module GitCrecord
     def self.toplevel_dir
       `git rev-parse --show-toplevel`.chomp
     end
-
-# void cd_toplevel() {
-#     const std::string toplevel_dir(get_toplevel_dir());
-#     logger() << "toplevel_dir: " << toplevel_dir << std::endl;
-#     chdir(toplevel_dir.c_str());
-#     char b[PATH_MAX];
-#     logger() << "cwd: " << getcwd(b, sizeof(b) / sizeof(*b)) << std::endl;
-# }
   end
 end
