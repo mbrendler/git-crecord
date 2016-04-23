246a7a4fec62587428f06e918fb55925a6f3765c
Maik Brendler <maik.brendler@injixo.com>
2016-04-24 00:31:17 +0200

    Start UI

--- lib/git_crecord.rb
+++ lib/git_crecord.rb
@@ -1,12 +1,13 @@
 require_relative 'git_crecord/git'
+require_relative 'git_crecord/hunks'
+require_relative 'git_crecord/ui'
 
 module GitCrecord
   module_function
 
   def main
     Dir.chdir(Git.toplevel_dir) do
-      puts Dir.pwd
-      puts Git.diff
+      UI.run(Hunks.parse(Git.diff))
       0
     end
   end
