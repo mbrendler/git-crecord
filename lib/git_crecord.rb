53d00a282b17343004a7a1536d11d078e020b103
Maik Brendler <maik.brendler@injixo.com>
2016-04-28 00:57:45 +0200

    Add --help option

--- lib/git_crecord.rb
+++ lib/git_crecord.rb
@@ -2,11 +2,15 @@ require_relative 'git_crecord/git'
 require_relative 'git_crecord/hunks'
 require_relative 'git_crecord/ui'
 require_relative 'git_crecord/version'
+require_relative 'git_crecord/ui/help_window'
 
 module GitCrecord
   def self.main(argv)
     if argv.include?('--version')
       puts VERSION
+      true
+    elsif argv.include?('--help') || argv.include?('-h')
+      help
       true
     else
       run
@@ -22,5 +26,17 @@ module GitCrecord
       return result.call == true if result.respond_to?(:call)
       true
     end
+  end
+
+  def self.help
+    puts <<EOS
+usage: git crecord [<options>]'
+
+  --version   - show version information'
+  -h          - this help message'
+
+  in-program commands:'
+#{UI::HelpWindow::CONTENT.gsub(/^/, '  ')}
+EOS
   end
 end
--- lib/git_crecord/ui/help_window.rb
+++ lib/git_crecord/ui/help_window.rb
@@ -4,26 +4,26 @@ module GitCrecord
   module UI
     module HelpWindow
       CONTENT = <<EOS.freeze
-q      - quit
-s      - stage selection and quit
-c      - commit selection and quit
-j / ↓  - down
-k / ↑  - up
-h / ←  - collapse hunk
-l / →  - expand
-f      - toggle fold
-g      - go to first line
-G      - go to last line
-SPACE  - toggle selection
-A      - toggle all selections
-?      - this help window
+  q      - quit
+  s      - stage selection and quit
+  c      - commit selection and quit
+  j / ↓  - down
+  k / ↑  - up
+  h / ←  - collapse hunk
+  l / →  - expand
+  f      - toggle fold
+  g      - go to first line
+  G      - go to last line
+  SPACE  - toggle selection
+  A      - toggle all selections
+  ?      - this help window
 EOS
 
       def self.show
         win = Curses::Window.new(height, width, 0, 0)
         win.box('|', '-')
         CONTENT.split("\n").each_with_index do |line, index|
-          win.setpos(index + 1, 3)
+          win.setpos(index + 1, 1)
           win.addstr(line)
         end
         win.getch
@@ -31,7 +31,7 @@ EOS
       end
 
       def self.width
-        CONTENT.lines.map(&:size).max + 5
+        CONTENT.lines.map(&:size).max + 3
       end
 
       def self.height
