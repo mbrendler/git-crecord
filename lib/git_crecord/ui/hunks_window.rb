584fb584a28974ab27d37045dbcbabf7f0b7e579
Maik Brendler <maik.brendler@injixo.com>
2016-04-28 19:56:44 +0200

    Reduce redraw calls

--- lib/git_crecord/ui/hunks_window.rb
+++ lib/git_crecord/ui/hunks_window.rb
@@ -149,14 +149,14 @@ module GitCrecord
       end
 
       def collapse
-        return if @highlighted.is_a?(Hunks::Line)
+        return if @highlighted.is_a?(Hunks::Line) || !@highlighted.expanded
         @highlighted.expanded = false
         update_visibles
         redraw
       end
 
       def expand
-        return if @highlighted.is_a?(Hunks::Line)
+        return if @highlighted.is_a?(Hunks::Line) || @highlighted.expanded
         @highlighted.expanded = true
         update_visibles
         @highlighted = @visibles[@visibles.index(@highlighted) + 1]
