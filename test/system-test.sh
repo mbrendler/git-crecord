ebf5bfc8a848e203b4d8cf4f2aa87118ee475266
Maik Brendler <maik.brendler@injixo.com>
2016-04-29 20:17:01 +0200

    Improve system-test output

--- test/system-test.sh
+++ test/system-test.sh
@@ -61,13 +61,13 @@ This is line 4.
 EOF
 
 
-echo "add all" # --------------------------------------------------------------
+echo "add all -----------------------------------------------------------------"
 run-git-crecord "s"
 assert-diff ""
 
 git reset
 
-echo "add first line" # -------------------------------------------------------
+echo "add first line ----------------------------------------------------------"
 run-git-crecord " lj s"
 assert-diff "+This is the second line.
 +This is line 3.
@@ -75,7 +75,7 @@ assert-diff "+This is the second line.
 
 git reset
 
-echo "add another line" # -----------------------------------------------------
+echo "add another line --------------------------------------------------------"
 run-git-crecord " ljjj s"
 assert-diff "+This is line 1.
 +This is the second line.
@@ -86,13 +86,13 @@ git ci -a -m "add some lines"
 
 sed -i '' '1,3d' a_file.txt
 
-echo "delete all lines" # -----------------------------------------------------
+echo "delete all lines --------------------------------------------------------"
 run-git-crecord "s"
 assert-diff ""
 
 git reset
 
-echo "delete one lines" # -----------------------------------------------------
+echo "delete one lines --------------------------------------------------------"
 run-git-crecord " ljj s"
 assert-diff "-This is line 1.
 -This is line 3."
@@ -118,27 +118,27 @@ sed -i '' '2s/.*/This is line 2./' a_file.txt
 sed -i '' '12s/.*/This is the tenth line./' a_file.txt
 sed -i '' '13s/.*/This is the eleventh line./' a_file.txt
 
-echo "multiple hunks" # -------------------------------------------------------
+echo "multiple hunks ----------------------------------------------------------"
 run-git-crecord "s"
 assert-diff ""
 
 git reset
 
-echo "add lines of second hunk" #----------------------------------------------
+echo "add lines of second hunk ------------------------------------------------"
 run-git-crecord " ljjj sq "
 assert-diff "-This is the second line.
 +This is line 2."
 
 git reset
 
-echo "add some lines of all hunks" #-------------------------------------------
+echo "add some lines of all hunks ---------------------------------------------"
 run-git-crecord " ljj jj jj j s"
 assert-diff "-This is the second line.
 -This is line 11."
 
 git reset
 
-echo "run git-crecord in a subdirectory directory" # --------------------------
+echo "run git-crecord in a subdirectory directory -----------------------------"
 mkdir -p "$REPO_DIR"/sub
 pushd "$REPO_DIR"/sub
 run-git-crecord "s"
@@ -147,7 +147,7 @@ popd # "$REPO_DIR"/sub
 
 git reset
 
-echo "add a untracked file"
+echo "add a untracked file ----------------------------------------------------"
 echo "b_file line 1" > b_file.txt
 run-git-crecord 'AG s'
 git commit -m "add b_file"
@@ -158,12 +158,12 @@ assert-diff '-This is the second line.
 +This is the tenth line.
 +This is the eleventh line.'
 
-echo "a not selected file" # --------------------------------------------------
+echo "a not selected file -----------------------------------------------------"
 echo "b_file line 2" >> b_file.txt
 run-git-crecord "j s"
 assert-diff "+b_file line 2"
 
-echo "add untracked file from untracked directory" # --------------------------
+echo "add untracked file from untracked directory -----------------------------"
 echo "a line" > "$REPO_DIR/sub/sub-file.txt"
 run-git-crecord "AG s"
 assert-diff "+b_file line 2"
