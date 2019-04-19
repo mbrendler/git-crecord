aa84f94f9d9fb5da9aa8ac41126793642ea76f95
Maik Brendler <maik.brendler@injixo.com>
2019-04-19 19:13:06 +0200

    Add StatusBar module

--- lib/git_crecord/ui/color.rb
+++ lib/git_crecord/ui/color.rb
@@ -9,7 +9,8 @@ module GitCrecord
         normal: 1,
         green: 2,
         red: 3,
-        hl: 4
+        hl: 4,
+        status_bar: 5
       }.freeze
 
       def self.init
@@ -19,6 +20,9 @@ module GitCrecord
         Curses.init_pair(MAP[:green], Curses::COLOR_GREEN, -1)
         Curses.init_pair(MAP[:red], Curses::COLOR_RED, -1)
         Curses.init_pair(MAP[:hl], Curses::COLOR_BLACK, Curses::COLOR_GREEN)
+        Curses.init_pair(
+          MAP[:status_bar], Curses::COLOR_BLACK, Curses::COLOR_BLUE
+        )
       end
 
       MAP.each_pair do |name, number|
---
+++ lib/git_crecord/ui/status_bar.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'curses'
+require_relative 'color'
+require_relative '../git'
+
+module GitCrecord
+  module UI
+    module StatusBar
+      def self.refresh(main_win)
+        write_left(main_win)
+        fill_to_eol
+        write_right(main_win)
+        win.refresh
+      end
+
+      def self.write_left(_main_win)
+        win.setpos(0, 0)
+        win.addstr(" #{branch}")
+      end
+
+      def self.write_right(main_win)
+        str = " #{main_win.highlight_position} "
+        win.setpos(0, [0, win.maxx - str.size].max)
+        win.addstr(str)
+      end
+
+      def self.fill_to_eol
+        fill_width = win.maxx - win.curx
+        win.addstr(' ' * fill_width) if fill_width.positive?
+      end
+
+      def self.win
+        @win ||= Curses::Window.new(1, Curses.cols, 0, 0).tap do |win|
+          win.attrset(Color.status_bar | Curses::A_BOLD)
+        end
+      end
+
+      def self.branch
+        @branch = Git.branch
+      end
+    end
+  end
+end
