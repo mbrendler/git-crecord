57293250dbd15031c11fc57b574517703fed57ec
Maik Brendler <maik.brendler@injixo.com>
2025-09-14 20:23:46 +0200

    Fix include directories

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-# export LIBGIT2=/opt/homebrew/opt/libgit2/lib/libgit2.a
-export LIBRARY_PATH=/opt/homebrew/opt/libgit2/lib
-export C_INCLUDE_PATH=/opt/homebrew/opt/libgit2/include
+export LIBRARY_PATH=/opt/homebrew/lib
+export C_INCLUDE_PATH=/opt/homebrew/include
 
 cc -g -std=c2x -Werror -Wall -Wextra -Wpedantic -O3 -o git-crecord \
   main.c \
