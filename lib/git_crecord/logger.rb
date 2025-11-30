b7ed183dcb0b7314ff8e96691537a77cc06dbd05
Maik Brendler <maik.brendler@injixo.com>
2025-11-30 05:32:33 +0100

    Remove old ruby code

--- lib/git_crecord/logger.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'logger'
-
-module GitCrecord
-  LOGGER = Logger.new(File.new(File.join(ENV['HOME'], '.git-crecord.log'), 'w'))
-  LOGGER.formatter = proc { |_severity, _datetime, _progname, msg| "#{msg}\n" }
-  LOGGER.level = Logger::INFO
-end
--- lib/git_crecord/quit_action.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module GitCrecord
-  class QuitAction < Proc
-    def ==(other)
-      other == :quit
-    end
-  end
-end
